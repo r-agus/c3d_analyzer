5aac18ed9d14d2b5604e417c8f49b6886d381790
Ruben Agustin <r.agustin@alumnos.upm.es>
2024-10-04 13:51:55 +0200

    added inspector

--- plugins/gui/src/lib.rs
+++ plugins/gui/src/lib.rs
@@ -1,17 +1,61 @@
-use bevy::prelude::*;
-use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use bevy::{input::common_conditions::input_toggle_active, prelude::*, window::PrimaryWindow};
+
+use bevy_egui::{egui, EguiContext, EguiPlugin};
+use bevy_inspector_egui::{bevy_inspector::hierarchy::SelectedEntities, quick::WorldInspectorPlugin, DefaultInspectorConfigPlugin};
 
 pub struct GUIPlugin;
 
 impl Plugin for GUIPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(EguiPlugin)
-            .add_systems(Update, ui_example_system);
+            .add_plugins(DefaultInspectorConfigPlugin)
+            .add_systems(
+                Update,
+                inspector_ui.run_if(input_toggle_active(true, KeyCode::Escape)),
+            );
     }
 }
 
-fn ui_example_system(mut contexts: EguiContexts) {
-    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
-        ui.label("world");
-    });
+fn inspector_ui(world: &mut World, mut selected_entities: Local<SelectedEntities>) {
+    let mut egui_context = world
+        .query_filtered::<&mut EguiContext, With<PrimaryWindow>>()
+        .single(world)
+        .clone();
+    egui::SidePanel::left("hierarchy")
+        .default_width(200.0)
+        .show(egui_context.get_mut(), |ui| {
+            egui::ScrollArea::both().show(ui, |ui| {
+                ui.heading("Hierarchy");
+
+                bevy_inspector_egui::bevy_inspector::hierarchy::hierarchy_ui(
+                    world,
+                    ui,
+                    &mut selected_entities,
+                );
+
+                ui.label("Press escape to toggle UI");
+                ui.allocate_space(ui.available_size());
+            });
+        });
+
+    egui::SidePanel::right("inspector")
+        .default_width(250.0)
+        .show(egui_context.get_mut(), |ui| {
+            egui::ScrollArea::both().show(ui, |ui| {
+                ui.heading("Inspector");
+
+                match selected_entities.as_slice() {
+                    &[entity] => {
+                        bevy_inspector_egui::bevy_inspector::ui_for_entity(world, entity, ui);
+                    }
+                    entities => {
+                        bevy_inspector_egui::bevy_inspector::ui_for_entities_shared_components(
+                            world, entities, ui,
+                        );
+                    }
+                }
+
+                ui.allocate_space(ui.available_size());
+            });
+        });
 }
