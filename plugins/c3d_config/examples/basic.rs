9df71ff69bc307a2b6098e00ffa50c4104fcf222
Ruben Agustin <r.agustin@alumnos.upm.es>
2024-10-14 19:02:04 +0200

    Refactor code to print the config map instead of the entire config file

--- plugins/c3d_config/examples/basic.rs
+++ plugins/c3d_config/examples/basic.rs
@@ -2,5 +2,5 @@ use config_plugin::*;
 
 fn main() {
     let config_file = parse_config("./assets/example.toml").unwrap();
-    println!("{:?}", config_file);
+    println!("{:?}", config_file.get_config_map());
 }
