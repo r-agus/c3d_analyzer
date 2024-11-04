fcca12683d7a3903baa5caf3c4c4360ae3ec4b0c
Ruben Agustin <r.agustin@alumnos.upm.es>
2024-11-04 11:16:43 +0100

    add bevy_metrics_dashboard to git

---
+++ plugins/gui/bevy_metrics_dashboard/examples/many_metrics.rs
@@ -0,0 +1,126 @@
+use bevy::prelude::*;
+use bevy_egui::EguiPlugin;
+use bevy_metrics_dashboard::{DashboardPlugin, DashboardWindow, RegistryPlugin};
+use metrics::{
+    counter, describe_counter, describe_gauge, describe_histogram, gauge, histogram, Unit,
+};
+use rand::Rng;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin)
+        .add_plugins(RegistryPlugin::default())
+        .add_plugins(DashboardPlugin)
+        .add_systems(Startup, (describe_metrics, create_dashboard))
+        .add_systems(Update, update_metrics)
+        .run();
+}
+
+fn describe_metrics() {
+    describe_counter!("foo", Unit::Count, "Lorem ipsum");
+    describe_gauge!("foo", Unit::Count, "Lorem ipsum");
+    describe_histogram!("foo", Unit::Count, "Lorem ipsum");
+    describe_counter!("bar", Unit::Count, "Lorem ipsum");
+    describe_gauge!("bar", Unit::Count, "Lorem ipsum");
+    describe_histogram!("bar", Unit::Count, "Lorem ipsum");
+    describe_counter!("baz", Unit::Count, "Lorem ipsum");
+    describe_gauge!("baz", Unit::Count, "Lorem ipsum");
+    describe_histogram!("baz", Unit::Count, "Lorem ipsum");
+    describe_counter!("bop", Unit::Count, "Lorem ipsum");
+    describe_gauge!("bop", Unit::Count, "Lorem ipsum");
+    describe_histogram!("bop", Unit::Count, "Lorem ipsum");
+    describe_counter!("doowop", Unit::Count, "Lorem ipsum");
+    describe_gauge!("doowop", Unit::Count, "Lorem ipsum");
+    describe_histogram!("doowop", Unit::Count, "Lorem ipsum");
+    describe_counter!("skibby", Unit::Count, "Lorem ipsum");
+    describe_gauge!("skibby", Unit::Count, "Lorem ipsum");
+    describe_histogram!("skibby", Unit::Count, "Lorem ipsum");
+    describe_counter!("dibby", Unit::Count, "Lorem ipsum");
+    describe_gauge!("dibby", Unit::Count, "Lorem ipsum");
+    describe_histogram!("dibby", Unit::Count, "Lorem ipsum");
+    describe_counter!("doop", Unit::Count, "Lorem ipsum");
+    describe_gauge!("doop", Unit::Count, "Lorem ipsum");
+    describe_histogram!("doop", Unit::Count, "Lorem ipsum");
+    describe_counter!("foo2", Unit::Count, "Lorem ipsum");
+    describe_gauge!("foo2", Unit::Count, "Lorem ipsum");
+    describe_histogram!("foo2", Unit::Count, "Lorem ipsum");
+    describe_counter!("bar2", Unit::Count, "Lorem ipsum");
+    describe_gauge!("bar2", Unit::Count, "Lorem ipsum");
+    describe_histogram!("bar2", Unit::Count, "Lorem ipsum");
+    describe_counter!("baz2", Unit::Count, "Lorem ipsum");
+    describe_gauge!("baz2", Unit::Count, "Lorem ipsum");
+    describe_histogram!("baz2", Unit::Count, "Lorem ipsum");
+    describe_counter!("bop2", Unit::Count, "Lorem ipsum");
+    describe_gauge!("bop2", Unit::Count, "Lorem ipsum");
+    describe_histogram!("bop2", Unit::Count, "Lorem ipsum");
+    describe_counter!("doowop2", Unit::Count, "Lorem ipsum");
+    describe_gauge!("doowop2", Unit::Count, "Lorem ipsum");
+    describe_histogram!("doowop2", Unit::Count, "Lorem ipsum");
+    describe_counter!("skibby2", Unit::Count, "Lorem ipsum");
+    describe_gauge!("skibby2", Unit::Count, "Lorem ipsum");
+    describe_histogram!("skibby2", Unit::Count, "Lorem ipsum");
+    describe_counter!("dibby2", Unit::Count, "Lorem ipsum");
+    describe_gauge!("dibby2", Unit::Count, "Lorem ipsum");
+    describe_histogram!("dibby2", Unit::Count, "Lorem ipsum");
+    describe_counter!("doop2", Unit::Count, "Lorem ipsum");
+    describe_gauge!("doop2", Unit::Count, "Lorem ipsum");
+    describe_histogram!("doop2", Unit::Count, "Lorem ipsum");
+}
+
+fn create_dashboard(mut commands: Commands) {
+    commands.spawn(DashboardWindow::new("Metrics Dashboard"));
+}
+
+fn update_metrics() {
+    let mut rng = rand::thread_rng();
+
+    histogram!("foo").record(rng.gen_range(0.0..10.0));
+    gauge!("foo").set(rng.gen_range(0.0..10.0));
+    counter!("foo").increment(rng.gen_range(0..10));
+    histogram!("bar").record(rng.gen_range(0.0..10.0));
+    gauge!("bar").set(rng.gen_range(0.0..10.0));
+    counter!("bar").increment(rng.gen_range(0..10));
+    histogram!("baz").record(rng.gen_range(0.0..10.0));
+    gauge!("baz").set(rng.gen_range(0.0..10.0));
+    counter!("baz").increment(rng.gen_range(0..10));
+    histogram!("bop").record(rng.gen_range(0.0..10.0));
+    gauge!("bop").set(rng.gen_range(0.0..10.0));
+    counter!("bop").increment(rng.gen_range(0..10));
+    histogram!("doowop").record(rng.gen_range(0.0..10.0));
+    gauge!("doowop").set(rng.gen_range(0.0..10.0));
+    counter!("doowop").increment(rng.gen_range(0..10));
+    histogram!("skibby").record(rng.gen_range(0.0..10.0));
+    gauge!("skibby").set(rng.gen_range(0.0..10.0));
+    counter!("skibby").increment(rng.gen_range(0..10));
+    histogram!("dibby").record(rng.gen_range(0.0..10.0));
+    gauge!("dibby").set(rng.gen_range(0.0..10.0));
+    counter!("dibby").increment(rng.gen_range(0..10));
+    histogram!("doop").record(rng.gen_range(0.0..10.0));
+    gauge!("doop").set(rng.gen_range(0.0..10.0));
+    counter!("doop").increment(rng.gen_range(0..10));
+    histogram!("foo2").record(rng.gen_range(0.0..10.0));
+    gauge!("foo2").set(rng.gen_range(0.0..10.0));
+    counter!("foo2").increment(rng.gen_range(0..10));
+    histogram!("bar2").record(rng.gen_range(0.0..10.0));
+    gauge!("bar2").set(rng.gen_range(0.0..10.0));
+    counter!("bar2").increment(rng.gen_range(0..10));
+    histogram!("baz2").record(rng.gen_range(0.0..10.0));
+    gauge!("baz2").set(rng.gen_range(0.0..10.0));
+    counter!("baz2").increment(rng.gen_range(0..10));
+    histogram!("bop2").record(rng.gen_range(0.0..10.0));
+    gauge!("bop2").set(rng.gen_range(0.0..10.0));
+    counter!("bop2").increment(rng.gen_range(0..10));
+    histogram!("doowop2").record(rng.gen_range(0.0..10.0));
+    gauge!("doowop2").set(rng.gen_range(0.0..10.0));
+    counter!("doowop2").increment(rng.gen_range(0..10));
+    histogram!("skibby2").record(rng.gen_range(0.0..10.0));
+    gauge!("skibby2").set(rng.gen_range(0.0..10.0));
+    counter!("skibby2").increment(rng.gen_range(0..10));
+    histogram!("dibby2").record(rng.gen_range(0.0..10.0));
+    gauge!("dibby2").set(rng.gen_range(0.0..10.0));
+    counter!("dibby2").increment(rng.gen_range(0..10));
+    histogram!("doop2").record(rng.gen_range(0.0..10.0));
+    gauge!("doop2").set(rng.gen_range(0.0..10.0));
+    counter!("doop2").increment(rng.gen_range(0..10));
+}
---
+++ plugins/gui/bevy_metrics_dashboard/src/lib.rs
@@ -0,0 +1,71 @@
+#![doc = include_str!("../README.md")]
+
+mod core_metrics_plugin;
+mod dashboard_plugin;
+mod dashboard_window;
+mod dropdown_list;
+pub mod namespace_tree;
+pub mod plots;
+pub mod registry;
+mod registry_plugin;
+mod ring;
+pub mod search_bar;
+
+#[cfg(feature = "render_metrics")]
+mod render_metrics_plugin;
+
+pub use core_metrics_plugin::CoreMetricsPlugin;
+pub use dashboard_plugin::DashboardPlugin;
+pub use dashboard_window::{CachedPlotConfigs, DashboardWindow, RequestPlot};
+pub use registry_plugin::{ClearBucketsSystem, RegistryPlugin};
+
+#[cfg(feature = "render_metrics")]
+pub use render_metrics_plugin::RenderMetricsPlugin;
+
+pub use bevy_egui;
+pub use metrics;
+pub use metrics_util;
+
+// TODO:
+// - dedup some of the code between gauge and counter widgets
+// - extract egui-only crate (with no bevy dep)
+// - don't assume there is a single egui context
+// - automatic significant figures
+// - automatically determine histogram bucket bounds
+// - search by label and description
+// - multiple metrics on one plot
+// - rename and save plots
+// - drag and drop plots (requires egui 0.26)
+
+use metrics::Unit;
+use metrics_util::MetricKind;
+
+fn metric_kind_str(kind: MetricKind) -> &'static str {
+    match kind {
+        MetricKind::Counter => "counter",
+        MetricKind::Gauge => "gauge",
+        MetricKind::Histogram => "histogram",
+    }
+}
+
+fn unit_str(unit: Unit) -> &'static str {
+    match unit {
+        Unit::Count => "count",
+        Unit::Percent => "%",
+        Unit::Seconds => "s",
+        Unit::Milliseconds => "ms",
+        Unit::Microseconds => "μs",
+        Unit::Nanoseconds => "ns",
+        Unit::Tebibytes => "TiB",
+        Unit::Gigibytes => "GiB",
+        Unit::Mebibytes => "MiB",
+        Unit::Kibibytes => "KiB",
+        Unit::Bytes => "B",
+        Unit::TerabitsPerSecond => "Tb/s",
+        Unit::GigabitsPerSecond => "Gb/s",
+        Unit::MegabitsPerSecond => "Mb/s",
+        Unit::KilobitsPerSecond => "Kb/s",
+        Unit::BitsPerSecond => "b/s",
+        Unit::CountPerSecond => "hz",
+    }
+}
