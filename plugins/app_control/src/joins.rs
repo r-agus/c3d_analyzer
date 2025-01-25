92004f0ff21e5f8af1bfe72cad9df22f6bde6bc3
Ruben Agustin <r.agustin@alumnos.upm.es>
2025-01-25 23:21:48 +0100

    Enhance join structure to include shape and add support for rectangular prism configuration

--- plugins/app_control/src/joins.rs
+++ plugins/app_control/src/joins.rs
@@ -3,8 +3,8 @@ use config_plugin::JoinShape;
 use crate::*;
 
 #[derive(Component)]
-/// This represents the joins between the points in the C3D file. It contains the labels of the points that are joined.
-pub struct Join(pub(crate) String, pub(crate) String);
+/// This represents the joins between the points in the C3D file. It contains the labels of the points that are joined and the shape of the join.
+pub struct Join(pub(crate) String, pub(crate) String, JoinShape);
 
 #[derive(Event)]
 pub enum JoinEvent {
@@ -22,10 +22,20 @@ pub(crate) fn spawn_joins_in_config(
     if config_file.get_config(current_config).is_some(){
         if let Some(joins) = config_file.get_config(current_config).unwrap().get_joins(){
             for (join, shape) in joins {
-                match shape {
-                    JoinShape::Line => line_config(current_config, config_file, commands, meshes, materials, join),
-                    JoinShape::Cylinder(radius) => cylinder_config(current_config, config_file, commands, meshes, materials, join, *radius),
-                    JoinShape::SemiCone(radius1, radius2) => semicone_config(current_config, config_file, commands, meshes, materials, join, *radius1, *radius2),
+                for i in 0..join.len() - 1 {
+                    let line_color = config_file.get_join_color(&join[i], &join[i+1], &current_config).unwrap_or(vec![0, 255, 0]);
+                    let join_material = standard_material_with_color(materials, line_color);
+                    let join_mesh = match shape {
+                        JoinShape::Line => line_config(current_config, config_file, meshes, &join[i], &join[i+1]),
+                        JoinShape::Cylinder(radius) => cylinder_config(meshes, *radius),
+                        JoinShape::SemiCone(radius1, radius2) => semicone_config(meshes, *radius1, *radius2),
+                        JoinShape::RectangularPrism(width, height, _) => rectangular_prism_config(meshes, *width, *height),
+                    };
+                    commands.spawn((
+                        Mesh3d(join_mesh),
+                        MeshMaterial3d(join_material),
+                        Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
+                        Join(join[i].clone(), join[i+1].clone(), shape.clone())));
                 }
             }
         }
@@ -35,76 +45,48 @@ pub(crate) fn spawn_joins_in_config(
 fn line_config(
     current_config: &str, 
     config_file: &ConfigFile, 
-    commands: &mut Commands<'_, '_>, 
     meshes: &mut ResMut<'_, Assets<Mesh>>, 
-    materials: &mut ResMut<'_, Assets<StandardMaterial>>, 
-    join: &Vec<String>
-){
-    for i in 0..join.len() - 1 {
-        let line_thickness = config_file.get_line_thickness(&join[i], &join[i+1], &current_config).unwrap_or(0.01) as f32;
-        let line_color = config_file.get_join_color(&join[i], &join[i+1], &current_config).unwrap_or(vec![0, 255, 0]);
-        let join_mesh =  meshes.add(
-                Cylinder::new(
-                    if line_thickness > 0.01 { line_thickness * 0.01 } else { 0.01 },
-                    1.0)
-            );
-        let join_material = standard_material_with_color(materials, line_color);
-
-        commands.spawn((
-            Mesh3d(join_mesh),
-            MeshMaterial3d(join_material),
-            Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
-            Join(join[i].clone(), join[i+1].clone())));
-    }
+    p1: &str,
+    p2: &str,
+) -> Handle<Mesh> {
+    let line_thickness = config_file.get_line_thickness(p1, p2, &current_config).unwrap_or(0.01) as f32;
+    let join_mesh =  meshes.add(
+    Cylinder::new(
+        if line_thickness > 0.01 { line_thickness * 0.01 } else { 0.01 },
+        1.0)
+    );
+    join_mesh
 }
 
 fn cylinder_config(
-    current_config: &str, 
-    config_file: &ConfigFile, 
-    commands: &mut Commands<'_, '_>, 
     meshes: &mut ResMut<'_, Assets<Mesh>>, 
-    materials: &mut ResMut<'_, Assets<StandardMaterial>>, 
-    join: &Vec<String>,
     radius: f64,
-){
-    for i in 0..join.len() - 1 {
-        let line_color = config_file.get_join_color(&join[i], &join[i+1], &current_config).unwrap_or(vec![0, 255, 0]);
-        let join_mesh =  meshes.add(Cylinder::new((radius * 0.01) as f32, 0.8));
-        let join_material = standard_material_with_color(materials, line_color);
-
-        commands.spawn((
-            Mesh3d(join_mesh),
-            MeshMaterial3d(join_material),
-            Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
-            Join(join[i].clone(), join[i+1].clone())));
-    }
+) -> Handle<Mesh> {
+    meshes.add(Cylinder::new((radius * 0.01) as f32, 0.95))
 }
 
 fn semicone_config(
-    current_config: &str, 
-    config_file: &ConfigFile, 
-    commands: &mut Commands<'_, '_>, 
     meshes: &mut ResMut<'_, Assets<Mesh>>, 
-    materials: &mut ResMut<'_, Assets<StandardMaterial>>, 
-    join: &Vec<String>,
     radius1: f64,
     radius2: f64,
-){
-    for i in 0..join.len() - 1 {
-        let line_color = config_file.get_join_color(&join[i], &join[i+1], &current_config).unwrap_or(vec![0, 255, 0]);
-        let frustrum_mesh = meshes.add(ConicalFrustum {
-            height: 0.8,
-            radius_top: (radius1 * 0.01) as f32,
-            radius_bottom: (radius2 * 0.01) as f32,
-        });
-        let join_material = standard_material_with_color(materials, line_color);
+) -> Handle<Mesh> {
+    meshes.add(ConicalFrustum {
+        height: 0.95,
+        radius_top: (radius1 * 0.01) as f32,
+        radius_bottom: (radius2 * 0.01) as f32,
+    })        
+}
 
-        commands.spawn((
-            Mesh3d(frustrum_mesh),
-            MeshMaterial3d(join_material),
-            Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
-            Join(join[i].clone(), join[i+1].clone())));
-    }
+fn rectangular_prism_config(
+    meshes: &mut ResMut<'_, Assets<Mesh>>, 
+    width: f64,
+    height: f64,
+) -> Handle<Mesh> {
+    meshes.add(
+        Extrusion::new(
+            Rectangle::new((width * 0.01) as f32, 0.95),  // rotate the rectangle to be vertical
+            (height * 0.01) as f32)
+    )
 }
 
 fn standard_material_with_color(materials: &mut ResMut<'_, Assets<StandardMaterial>>, line_color: Vec<u8>) -> Handle<StandardMaterial> {
@@ -136,8 +118,22 @@ pub fn represent_joins(
             for (mut transform, join) in joins_query.iter_mut() {
                 let marker1 = get_marker_position_on_frame(&join.0, &markers_query);
                 let marker2 = get_marker_position_on_frame(&join.1, &markers_query);
-                match (marker1, marker2) {
-                    (Some(marker1), Some(marker2)) => {
+                let orientation_option = match &join.2 {
+                    JoinShape::RectangularPrism(_, _, option) => option.as_ref(),
+                    _ => None
+                };
+                match (marker1, marker2, orientation_option) {
+                    (Some(marker1), Some(marker2), Some(direction)) => {
+                        let position = (marker1 + marker2) / 2.0;
+                        let length = (marker1 - marker2).length();
+                        let direction = get_marker_position_on_frame(&direction.get(&join.0).unwrap_or(&("".to_string(), false)).0, &markers_query);
+                        let rotation = Quat::from_rotation_arc(Vec3::Y, direction.unwrap_or(Vec3::Y));
+                        let scale = Vec3::new(0.5, length, 0.5);
+                        transform.translation = position;
+                        transform.rotation = rotation;
+                        transform.scale = scale;
+                    }
+                    (Some(marker1), Some(marker2), None) => {
                         let position = (marker1 + marker2) / 2.0;
                         let length = (marker1 - marker2).length();
                         let direction = (marker1 - marker2).normalize();
