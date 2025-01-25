682e4bdb96bc684015b548aa6345590f6ef364d4
Ruben Agustin <r.agustin@alumnos.upm.es>
2025-01-25 17:25:38 +0100

    Refactor join handling to support multiple shapes with dedicated configuration functions for line, cylinder, and semicone joins

--- plugins/app_control/src/joins.rs
+++ plugins/app_control/src/joins.rs
@@ -1,3 +1,5 @@
+use config_plugin::JoinShape;
+
 use crate::*;
 
 #[derive(Component)]
@@ -18,36 +20,106 @@ pub(crate) fn spawn_joins_in_config(
     materials: &mut ResMut<Assets<StandardMaterial>>,
 ){
     if config_file.get_config(current_config).is_some(){
-        if let Some((joins, _)) = config_file.get_config(current_config).unwrap().get_joins(){
-            for join in joins {
-                for i in 0..join.len() - 1 {
-                    let line_thickness = config_file.get_line_thickness(&join[i], &join[i+1], &current_config).unwrap_or(0.01) as f32;
-                    let line_color = config_file.get_join_color(&join[i], &join[i+1], &current_config).unwrap_or(vec![0, 255, 0]);
-                    let join_mesh =  meshes.add(
-                            Cylinder::new(
-                                if line_thickness > 0.01 { line_thickness * 0.01 } else { 0.01 },
-                                1.0)
-                        );
-                    let join_material = materials.add(StandardMaterial {
-                        base_color: if line_color.len() == 3 {
-                                        Color::srgb_u8(line_color[0], line_color[1],line_color[2])
-                                    } else if line_color.len() == 4 {
-                                        Color::srgba_u8(line_color[0], line_color[1], line_color[2], line_color[3])
-                                    } else{
-                                        Color::srgb_u8(0, 127, 0)
-                                    },
-                        ..default()
-                    });
-
-                    commands.spawn((
-                        Mesh3d(join_mesh),
-                        MeshMaterial3d(join_material),
-                        Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
-                        Join(join[i].clone(), join[i+1].clone())));
+        if let Some(joins) = config_file.get_config(current_config).unwrap().get_joins(){
+            for (join, shape) in joins {
+                match shape {
+                    JoinShape::Line => line_config(current_config, config_file, commands, meshes, materials, join),
+                    JoinShape::Cylinder(radius) => cylinder_config(current_config, config_file, commands, meshes, materials, join, *radius),
+                    JoinShape::SemiCone(radius1, radius2) => semicone_config(current_config, config_file, commands, meshes, materials, join, *radius1, *radius2),
                 }
             }
         }
     }
+}
+
+fn line_config(
+    current_config: &str, 
+    config_file: &ConfigFile, 
+    commands: &mut Commands<'_, '_>, 
+    meshes: &mut ResMut<'_, Assets<Mesh>>, 
+    materials: &mut ResMut<'_, Assets<StandardMaterial>>, 
+    join: &Vec<String>
+){
+    for i in 0..join.len() - 1 {
+        let line_thickness = config_file.get_line_thickness(&join[i], &join[i+1], &current_config).unwrap_or(0.01) as f32;
+        let line_color = config_file.get_join_color(&join[i], &join[i+1], &current_config).unwrap_or(vec![0, 255, 0]);
+        let join_mesh =  meshes.add(
+                Cylinder::new(
+                    if line_thickness > 0.01 { line_thickness * 0.01 } else { 0.01 },
+                    1.0)
+            );
+        let join_material = standard_material_with_color(materials, line_color);
+
+        commands.spawn((
+            Mesh3d(join_mesh),
+            MeshMaterial3d(join_material),
+            Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
+            Join(join[i].clone(), join[i+1].clone())));
+    }
+}
+
+fn cylinder_config(
+    current_config: &str, 
+    config_file: &ConfigFile, 
+    commands: &mut Commands<'_, '_>, 
+    meshes: &mut ResMut<'_, Assets<Mesh>>, 
+    materials: &mut ResMut<'_, Assets<StandardMaterial>>, 
+    join: &Vec<String>,
+    radius: f64,
+){
+    for i in 0..join.len() - 1 {
+        let line_color = config_file.get_join_color(&join[i], &join[i+1], &current_config).unwrap_or(vec![0, 255, 0]);
+        let join_mesh =  meshes.add(Cylinder::new((radius * 0.01) as f32, 0.8));
+        let join_material = standard_material_with_color(materials, line_color);
+
+        commands.spawn((
+            Mesh3d(join_mesh),
+            MeshMaterial3d(join_material),
+            Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
+            Join(join[i].clone(), join[i+1].clone())));
+    }
+}
+
+fn semicone_config(
+    current_config: &str, 
+    config_file: &ConfigFile, 
+    commands: &mut Commands<'_, '_>, 
+    meshes: &mut ResMut<'_, Assets<Mesh>>, 
+    materials: &mut ResMut<'_, Assets<StandardMaterial>>, 
+    join: &Vec<String>,
+    radius1: f64,
+    radius2: f64,
+){
+    for i in 0..join.len() - 1 {
+        let line_color = config_file.get_join_color(&join[i], &join[i+1], &current_config).unwrap_or(vec![0, 255, 0]);
+        let frustrum_mesh = meshes.add(ConicalFrustum {
+            height: 0.8,
+            radius_top: (radius1 * 0.01) as f32,
+            radius_bottom: (radius2 * 0.01) as f32,
+        });
+        let join_material = standard_material_with_color(materials, line_color);
+
+        commands.spawn((
+            Mesh3d(frustrum_mesh),
+            MeshMaterial3d(join_material),
+            Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
+            Join(join[i].clone(), join[i+1].clone())));
+    }
+}
+
+fn standard_material_with_color(materials: &mut ResMut<'_, Assets<StandardMaterial>>, line_color: Vec<u8>) -> Handle<StandardMaterial> {
+    let join_material = materials.add(StandardMaterial {
+        base_color: 
+            if line_color.len() == 3 {
+                Color::srgb_u8(line_color[0], line_color[1],line_color[2])
+            } else if line_color.len() == 4 {
+                Color::srgba_u8(line_color[0], line_color[1], line_color[2], line_color[3])
+            } else{
+                Color::srgb_u8(0, 127, 0)
+            },
+        ..default()
+    });
+    join_material
 }
 
 pub fn represent_joins(
